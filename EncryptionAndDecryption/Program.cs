e729915e6bf0a6ecc91d820900d56eab815ac822
zahidul islam <zahidulislam5594@gmail.com>
2020-03-10 07:07:42 +0600

    Add project files.

---
+++ EncryptionAndDecryption/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionAndDecryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var encryption = CryptoEngine.Encrypt(input);
+            var decryption = CryptoEngine.Decrypt(encryption);
+            Console.WriteLine(encryption);
+            Console.WriteLine(decryption);  
+        }
+    }
+}
